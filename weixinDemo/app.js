c97257b13849b93f1da524d075e6ca4685e09fa0
songxianling <360198419@qq.com>
2018-04-11 16:12:01 +0800

    双向数据绑定分析

--- weixinDemo/app.js
+++
@@ -1,1 +0,0 @@
-import wxbarcode from "wxbarcode";
--- weixinDemo/pages/welcome/welcome.js
+++
@@ -1,9 +0,0 @@
-var app = getApp();
-
-Page({
-    onLoad() {
-        wxbarcode.barcode('barcode', '1234567890123456789', 680, 200);
-        alert(1);
-    }
-   
-})
