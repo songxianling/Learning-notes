747172f1d3a07d4dbada2cb0f7ba53475b691053
songxianling <360198419@qq.com>
2018-05-31 14:49:00 +0800

    解析url+浏览器存储

---
+++ try/js/浏览器存储.js
@@ -0,0 +1,38 @@
+
+export function getInfo() {
+    return localStorage.getItem(userInfo)
+  }
+  
+  export function setInfo(username) {
+    return localStorage.setItem(userInfo, username)
+  }
+  
+  export function removeInfo() {
+    return localStorage.removeItem(userInfo)
+  }
+  /**
+   * 存储localStorage
+   */
+  export const setStore = (name, content) => {
+      if (!name) return;
+      if (typeof content !== 'string') {
+          content = JSON.stringify(content);
+      }
+      window.localStorage.setItem(name, content);
+  }
+  
+  /**
+   * 获取localStorage
+   */
+  export const getStore = name => {
+      if (!name) return;
+      return window.localStorage.getItem(name);
+  }
+  
+  /**
+   * 删除localStorage
+   */
+  export const removeStore = name => {
+      if (!name) return;
+      window.localStorage.removeItem(name);
+  }
