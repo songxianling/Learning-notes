b54b67175fafa88fdc62c17cc738ec4d16b761cd
songxianling <360198419@qq.com>
2018-06-01 17:07:23 +0800

    remjs

--- try/js/rem.js
+++ try/js/rem.js
@@ -4,6 +4,7 @@
         recalc = function () {
             var clientWidth = docEl.clientWidth;
             if (clientWidth === undefined) return;
+            // 设置100   750  就是按照750的设计图（iphone6） 量的多少写多少就好了
             docEl.style.fontSize = 100 * (clientWidth / 750) + 'px';
         };
     if (doc.addEventListener === undefined) return;
