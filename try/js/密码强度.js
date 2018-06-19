d80f0129824587e3f8490812b5d891a8e17fc128
songxianling <360198419@qq.com>
2018-06-19 15:29:21 +0800

    手机端

---
+++ try/js/密码强度.js
@@ -0,0 +1,22 @@
+let checkPwd = (str) => {
+    var nowLv = 0;
+    if (str.length < 6) {
+        return nowLv
+    }
+    if (/[0-9]/.test(str)) {
+        nowLv++
+    }
+    if (/[a-z]/.test(str)) {
+        nowLv++
+    }
+    if (/[A-Z]/.test(str)) {
+        nowLv++
+    }
+    if (/[\.|-|_]/.test(str)) {
+        nowLv++
+    }
+    return nowLv;
+}
+
+
+console.log(checkPwd('123abc123'));
